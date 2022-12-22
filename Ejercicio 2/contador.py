c5ffab54998d7cc8a7ef195cdc7b8fc995100f19
crltsnch <crltsnch@myuax.com>
2022-12-22 19:57:41 +0100

    ejercicio 2

--- Ejercicio 2/contador.py
+++ Ejercicio 2/contador.py
@@ -1,9 +1,24 @@
 import sys
 
 def main():
-    fichero = open('contador.txt')
+    fichero = open('contador.txt', 'a+')
     contenido = fichero.readline()
 
-    if len(contenido)== 0:
-        contenido=0
-    
+    if len(contenido) == 0:
+        contenido = 0
+        fichero.write(contenido)
+    fichero.close()
+
+    try:
+        contador = int(contenido)
+        if len(sys.argv) == 2:
+            if sys.argv[1] == 'inc':
+                contador =+ 1
+            elif sys.argv[1] == 'dec':
+                contador -= 1
+        print(contador)
+        fichero = open('contador.txt', 'w')
+        fichero.write(str(contador))
+        fichero.close()
+    except ValueError:
+        print('Error: Fichero corrupto')
